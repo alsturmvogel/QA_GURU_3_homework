487f25cf7dd45204f57f0a86f963cd2892f2db43
Alena Gushcha <g.aliena@mail.ru>
2026-02-11 20:21:11 +0300

    Добавила тест на открытия страницы github.com и его проверки

---
+++ test_second.py
@@ -0,0 +1,23 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture
+def driver():
+    opts = Options()
+
+    opts.add_argument("--headless=new")
+    opts.add_argument("--window-size=1280,900")
+    driver = webdriver.Chrome(options=opts)
+    yield driver
+
+    driver.quit()
+
+
+def test_selenium_web(driver):
+    url = "https://github.com/"
+    driver.get(url)
+    assert driver.title == "GitHub · Change is constant. GitHub keeps you ahead. · GitHub"
+    assert driver.current_url == url
